c6cd14c4242aa1d59707923650afafa93f7ad2b1
Jóhan Davidsen <johan.davidsen@fjakkarin.com>
2017-02-22 17:01:30 +0100

    Added the Contributions component to the fjakkarin-app project

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Contributions } from './containers/contributions'
 
 // Used for development, should be removed from production.
 if (module.hot) {
   module.hot.accept()
 }
 
-// ReactDOM.render(<Activity />, document.getElementById('timeline-outer'))
+ReactDOM.render(<Contributions />, document.getElementById('footer-container-top'))
